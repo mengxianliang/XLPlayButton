1307ceda7b8a28e34e7e70d773d03037b991e35e
mengxianliang <mengxianliangok@163.com>
2017-08-09 16:35:48 +0800

    优化代码接口
    去除冗余重复代码

--- XLPlayButtonExample/XLPlayButton/XLPlayButton.m
+++ XLPlayButtonExample/XLPlayButton/XLPlayButton.m
@@ -8,11 +8,18 @@
 
 #import "XLPlayButton.h"
 
+//线条宽度
 static CGFloat lineWidth = 15.0f;
-static CGFloat checkDuration = 0.5f;
+//其它动画时长
+static CGFloat animationDuration = 0.5f;
+//位移动画时长
 static CGFloat positionDuration = 0.3;
-#define BlueColor [UIColor colorWithRed:16/255.0 green:142/255.0 blue:233/255.0 alpha:1]
-
+//线条颜色
+#define LineColor [UIColor colorWithRed:16/255.0 green:142/255.0 blue:233/255.0 alpha:1]
+//三角动画名称
+#define TriangleAnimation @"TriangleAnimation"
+#define leftLineAnimation @"leftLineAnimation"
+#define RightLineAnimation @"RightLineAnimation"
 
 @interface XLPlayButton ()<CAAnimationDelegate> {
     
@@ -53,6 +60,9 @@ static CGFloat positionDuration = 0.3;
     [self addCircleLayer];
 }
 
+#pragma mark -
+#pragma mark 添加动画层
+
 /**
  添加左侧竖线层
  */
@@ -66,7 +76,7 @@ static CGFloat positionDuration = 0.3;
     _leftLineLayer = [CAShapeLayer layer];
     _leftLineLayer.path = path.CGPath;
     _leftLineLayer.fillColor = [UIColor clearColor].CGColor;
-    _leftLineLayer.strokeColor = BlueColor.CGColor;
+    _leftLineLayer.strokeColor = LineColor.CGColor;
     _leftLineLayer.lineWidth = lineWidth;
     _leftLineLayer.lineCap = kCALineCapRound;
     _leftLineLayer.lineJoin = kCALineJoinRound;
@@ -89,7 +99,7 @@ static CGFloat positionDuration = 0.3;
     _triangleLayer = [CAShapeLayer layer];
     _triangleLayer.path = path.CGPath;
     _triangleLayer.fillColor = [UIColor clearColor].CGColor;
-    _triangleLayer.strokeColor = BlueColor.CGColor;
+    _triangleLayer.strokeColor = LineColor.CGColor;
     _triangleLayer.lineWidth = lineWidth;
     _triangleLayer.lineCap = kCALineCapButt;
     _triangleLayer.lineJoin = kCALineJoinRound;
@@ -111,14 +121,14 @@ static CGFloat positionDuration = 0.3;
     _rightLineLayer = [CAShapeLayer layer];
     _rightLineLayer.path = path.CGPath;
     _rightLineLayer.fillColor = [UIColor clearColor].CGColor;
-    _rightLineLayer.strokeColor = BlueColor.CGColor;
+    _rightLineLayer.strokeColor = LineColor.CGColor;
     _rightLineLayer.lineWidth = lineWidth;
     _rightLineLayer.lineCap = kCALineCapRound;
     _rightLineLayer.lineJoin = kCALineJoinRound;
     [self.layer addSublayer:_rightLineLayer];
 }
 /**
- 添加画弧层
+ 添加弧线过渡层
  */
 - (void)addCircleLayer {
     
@@ -131,7 +141,7 @@ static CGFloat positionDuration = 0.3;
     _circleLayer = [CAShapeLayer layer];
     _circleLayer.path = path.CGPath;
     _circleLayer.fillColor = [UIColor clearColor].CGColor;
-    _circleLayer.strokeColor = BlueColor.CGColor;
+    _circleLayer.strokeColor = LineColor.CGColor;
     _circleLayer.lineWidth = lineWidth;
     _circleLayer.lineCap = kCALineCapRound;
     _circleLayer.lineJoin = kCALineJoinRound;
@@ -139,31 +149,21 @@ static CGFloat positionDuration = 0.3;
     [self.layer addSublayer:_circleLayer];
 }
 
-/**
- 位移变化动画
- */
--(void)positionAnimationOfLayer:(CALayer*)layer :(CGFloat)yChange duration:(CGFloat)duration {
-    CABasicAnimation *triangleAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-    triangleAnimation.duration = duration/2;
-    triangleAnimation.fromValue = @(layer.position.y);
-    triangleAnimation.toValue = @(layer.position.y + yChange);
-    triangleAnimation.autoreverses = YES;
-    [triangleAnimation setValue:@"positionKey" forKey:@"animationName"];
-    [layer addAnimation:triangleAnimation forKey:nil];
-}
+#pragma mark -
+#pragma mark 动画执行方法
+
 
 /**
  执行正向动画，即暂停-》播放
  */
-
 - (void)actionPositiveAnimation {
     [self triangleAnimationFrome:0 to:1];
     [self rightLineAnimationFrome:1 to:0];
     [self circleEndAnimationFrome:0 to:1];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  checkDuration*0.25 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  animationDuration*0.25 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
         [self circleStartAnimationFrome:0 to:1];
     });
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  checkDuration*0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  animationDuration*0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
         [self leftLineAnimationFrome:1 to:0];
     });
 }
@@ -175,30 +175,35 @@ static CGFloat positionDuration = 0.3;
     [self triangleAnimationFrome:1 to:0];
     [self leftLineAnimationFrome:0 to:1];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  checkDuration*0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  animationDuration*0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
         [self circleStartAnimationFrome:1 to:0];
     });
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  checkDuration*0.75 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  animationDuration*0.75 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
         [self rightLineAnimationFrome:0 to:1];
         [self circleEndAnimationFrome:1 to:0];
     });
 }
 
 /**
+ 位移变化动画
+ */
+-(void)positionAnimationOfLayer:(CALayer*)layer :(CGFloat)yChange duration:(CGFloat)duration {
+    CABasicAnimation *positionAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    positionAnimation.duration = duration/2;
+    positionAnimation.fromValue = @(layer.position.y);
+    positionAnimation.toValue = @(layer.position.y + yChange);
+    positionAnimation.autoreverses = YES;
+    [positionAnimation setValue:@"positionKey" forKey:@"animationName"];
+    [layer addAnimation:positionAnimation forKey:nil];
+}
+
+
+/**
  三角形动画
  */
 - (void)triangleAnimationFrome:(CGFloat)fromeValue to:(CGFloat)toValue {
-    
-    CABasicAnimation *triangleAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    triangleAnimation.duration = checkDuration;
-    triangleAnimation.fromValue = @(fromeValue);
-    triangleAnimation.toValue = @(toValue);
-    triangleAnimation.fillMode = kCAFillModeForwards;
-    triangleAnimation.removedOnCompletion = NO;
-    triangleAnimation.delegate = self;
-    [triangleAnimation setValue:@"triangleAnimation" forKey:@"animationName"];
-    [_triangleLayer addAnimation:triangleAnimation forKey:nil];
+    [self strokeEndAnimationFrom:fromeValue to:toValue onLayer:_triangleLayer name:TriangleAnimation duration:animationDuration delegate:self];
 }
 
 /**
@@ -206,30 +211,14 @@ static CGFloat positionDuration = 0.3;
  */
 - (void)leftLineAnimationFrome:(CGFloat)fromeValue to:(CGFloat)toValue {
     
-    CABasicAnimation *leftLineAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    leftLineAnimation.duration = checkDuration/2;
-    leftLineAnimation.fromValue = @(fromeValue);
-    leftLineAnimation.toValue = @(toValue);
-    leftLineAnimation.fillMode = kCAFillModeForwards;
-    leftLineAnimation.removedOnCompletion = NO;
-    [leftLineAnimation setValue:@"leftLineAnimation" forKey:@"animationName"];
-    [_leftLineLayer addAnimation:leftLineAnimation forKey:nil];
+    [self strokeEndAnimationFrom:fromeValue to:toValue onLayer:_leftLineLayer name:leftLineAnimation duration:animationDuration/2 delegate:nil];
 }
 
 /**
  右侧竖线动画
  */
 - (void)rightLineAnimationFrome:(CGFloat)fromeValue to:(CGFloat)toValue {
-    
-    CABasicAnimation *rightLineAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    rightLineAnimation.duration = checkDuration/4;
-    rightLineAnimation.fromValue = @(fromeValue);
-    rightLineAnimation.toValue = @(toValue);
-    rightLineAnimation.fillMode = kCAFillModeForwards;
-    rightLineAnimation.removedOnCompletion = NO;
-    [rightLineAnimation setValue:@"rightLineAnimation" forKey:@"animationName"];
-    rightLineAnimation.delegate = self;
-    [_rightLineLayer addAnimation:rightLineAnimation forKey:nil];
+    [self strokeEndAnimationFrom:fromeValue to:toValue onLayer:_rightLineLayer name:RightLineAnimation duration:animationDuration/4 delegate:self];
 }
 
 
@@ -239,37 +228,32 @@ static CGFloat positionDuration = 0.3;
  */
 - (void)circleEndAnimationFrome:(CGFloat)fromeValue to:(CGFloat)toValue {
     
-    CABasicAnimation *leftLineAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    leftLineAnimation.duration = checkDuration/4;
-    leftLineAnimation.fromValue = @(fromeValue);
-    leftLineAnimation.toValue = @(toValue);
-    leftLineAnimation.fillMode = kCAFillModeForwards;
-    leftLineAnimation.removedOnCompletion = NO;
-    [leftLineAnimation setValue:@"leftLineAnimation" forKey:@"animationName"];
-    [_circleLayer addAnimation:leftLineAnimation forKey:nil];
+    [self strokeEndAnimationFrom:fromeValue to:toValue onLayer:_circleLayer name:nil duration:animationDuration/4 delegate:nil];
 }
 
 /**
  画弧改变起始位置动画
  */
 - (void)circleStartAnimationFrome:(CGFloat)fromeValue to:(CGFloat)toValue {
-    CABasicAnimation *leftLineAnimation = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
-    leftLineAnimation.duration = checkDuration/4;
-    leftLineAnimation.fromValue = @(fromeValue);
-    leftLineAnimation.toValue = @(toValue);
-    leftLineAnimation.fillMode = kCAFillModeForwards;
-    leftLineAnimation.removedOnCompletion = NO;
-    [leftLineAnimation setValue:@"leftLineAnimation" forKey:@"animationName"];
-    [_circleLayer addAnimation:leftLineAnimation forKey:nil];
+    CABasicAnimation *circleAnimation = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
+    circleAnimation.duration = animationDuration/4;
+    circleAnimation.fromValue = @(fromeValue);
+    circleAnimation.toValue = @(toValue);
+    circleAnimation.fillMode = kCAFillModeForwards;
+    circleAnimation.removedOnCompletion = NO;
+    [circleAnimation setValue:@"circleAnimation" forKey:@"animationName"];
+    [_circleLayer addAnimation:circleAnimation forKey:nil];
 }
 
 
 #pragma mark -
 #pragma mark 动画开始、结束代理方法
+
+//为了避免动画结束回到原点后会有一个原点显示在屏幕上需要做一些处理，就是改变layer的lineCap属性
 -(void)animationDidStart:(CAAnimation *)anim {
     NSString *name = [anim valueForKey:@"animationName"];
-    bool isTriangle = [name isEqualToString:@"triangleAnimation"];
-    bool isRightLine = [name isEqualToString:@"rightLineAnimation"];
+    bool isTriangle = [name isEqualToString:TriangleAnimation];
+    bool isRightLine = [name isEqualToString:RightLineAnimation];
     if (isTriangle) {
         _triangleLayer.lineCap = kCALineCapRound;
     }else if (isRightLine){
@@ -279,8 +263,8 @@ static CGFloat positionDuration = 0.3;
 
 -(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
     NSString *name = [anim valueForKey:@"animationName"];
-    bool isTriangle = [name isEqualToString:@"triangleAnimation"];
-    bool isRightLine = [name isEqualToString:@"rightLineAnimation"];
+    bool isTriangle = [name isEqualToString:TriangleAnimation];
+    bool isRightLine = [name isEqualToString:RightLineAnimation];
     if (_buttonState == XLPlayButtonStatePlay && isRightLine) {
         _rightLineLayer.lineCap = kCALineCapButt;
     }
@@ -292,17 +276,17 @@ static CGFloat positionDuration = 0.3;
 #pragma mark -
 #pragma mark 其他方法
 /**
- 执行strokeEnd动画
+ 通用执行strokeEnd动画
  */
-- (CABasicAnimation *)strokeEndAnimationFrom:(CGFloat)fromeValue to:(CGFloat)toValue onLayer:(CALayer *)layer name:(NSString*)animationName {
+- (CABasicAnimation *)strokeEndAnimationFrom:(CGFloat)fromeValue to:(CGFloat)toValue onLayer:(CALayer *)layer name:(NSString*)animationName duration:(CGFloat)duration delegate:(id)delegate {
     CABasicAnimation *strokeEndAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    strokeEndAnimation.duration = checkDuration/4;
+    strokeEndAnimation.duration = duration;
     strokeEndAnimation.fromValue = @(fromeValue);
     strokeEndAnimation.toValue = @(toValue);
     strokeEndAnimation.fillMode = kCAFillModeForwards;
     strokeEndAnimation.removedOnCompletion = NO;
     [strokeEndAnimation setValue:animationName forKey:@"animationName"];
-    strokeEndAnimation.delegate = self;
+    strokeEndAnimation.delegate = delegate;
     [layer addAnimation:strokeEndAnimation forKey:nil];
     return strokeEndAnimation;
 }
@@ -328,13 +312,13 @@ static CGFloat positionDuration = 0.3;
         //先执行画弧、画三角动画
         [self actionInverseAnimation];
         //在执行竖线位移动画，结束动动画要比开始动画块
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  checkDuration * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  animationDuration * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
             [self positionAnimationOfLayer:_leftLineLayer :20 duration:positionDuration*0.7];
             [self positionAnimationOfLayer:_rightLineLayer :-20 duration:positionDuration*0.7];
         });
     }
     //更新动画执行状态
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  (positionDuration + checkDuration) * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,  (positionDuration + animationDuration) * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
         _isAnimating = false;
     });
 
