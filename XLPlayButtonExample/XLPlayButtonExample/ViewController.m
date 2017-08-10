613345569e26edb07ae6163cbc80d46e0ba60317
mengxianliang <mengxianliangok@163.com>
2017-08-10 19:31:00 +0800

    666

--- XLPlayButtonExample/XLPlayButtonExample/ViewController.m
+++ XLPlayButtonExample/XLPlayButtonExample/ViewController.m
@@ -26,7 +26,7 @@
     _iQiYiPlayButton = [[iQiYiPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:iQiYiPlayButtonStatePlay];
     _iQiYiPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/3);
     [_iQiYiPlayButton addTarget:self action:@selector(iQiYiPlayMethod) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:_iQiYiPlayButton];
+    [self.view addSubview:_iQiYiPlayButton];
     
     //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
     _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
