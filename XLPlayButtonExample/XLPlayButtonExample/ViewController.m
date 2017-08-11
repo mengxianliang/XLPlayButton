ce5f6b31c392ed41d2c2197499f1e93f59005e77
mengxianliang <mengxianliangok@163.com>
2017-08-11 10:48:29 +0800

    666

--- XLPlayButtonExample/XLPlayButtonExample/ViewController.m
+++ XLPlayButtonExample/XLPlayButtonExample/ViewController.m
@@ -30,7 +30,7 @@
     
     //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
     _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
-    _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/2);
+    _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height*2/3);
     [_youKuPlayButton addTarget:self action:@selector(youKuPlayMethod) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_youKuPlayButton];
 }
